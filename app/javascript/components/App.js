62109b06c514d0abf5eecc82193d3662834a6d5d
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-04-16 22:40:50 -0700

    initial commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Button></Button>
+      </>
+    )
+  }
+}
+
+export default App
